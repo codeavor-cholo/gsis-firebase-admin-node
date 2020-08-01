510d633234805ce05f672997b2da83594e9c34ce
Cholz Del Rosario <chomigz12@gmail.com>
2020-08-01 19:26:12 +0800

    cors

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}),cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-})
 
 require('./app/routes')(app, {});
 
