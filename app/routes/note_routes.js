4e6fd6e2861eb53aae765a04fd714de647b9b954
Cholz Del Rosario <chomigz12@gmail.com>
2020-08-05 18:28:11 +0800

    delete user

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -33,4 +33,21 @@ module.exports = function(app) {
               res.send('Error updating user')
             });
     })
+
+    app.post('/deleteUser', (req, res) =>{
+      // create note here
+      console.log('request body', req.body )
+      console.log('request', req)
+
+      admin.auth().deleteUser(req.body.uid)
+          .then(function(userRecord) {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log('Successfully updated user', userRecord.toJSON());
+            res.send('Successfully updated user')
+          })
+          .catch(function(error) {
+            console.log('Error updating user:', error);
+            res.send('Error updating user')
+          });
+  })
 }
